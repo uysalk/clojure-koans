772fb3578fd9a30fc5b31f901c50dfddedb9a9d0
Uysal KARA <uysal.kara@careem.com>
2018-06-14 01:39:57 +0200

    refs

--- src/koans/13_creating_functions.clj
+++ src/koans/13_creating_functions.clj
@@ -6,12 +6,12 @@
 (meditations
   "One may know what they seek by knowing what they do not seek"
   (= [true false true] (let [not-a-symbol? (complement symbol?)]
-                  (map not-a-symbol? [:a 'b "c"])))
+                         (map not-a-symbol? [:a 'b "c"])))
 
   "Praise and 'complement' may help you separate the wheat from the chaff"
   (= [:wheat "wheat" 'wheat]
-       (let [not-nil? (complement nil?)]
-         (filter not-nil? [nil :wheat nil "wheat" nil 'wheat nil])))
+     (let [not-nil? (complement nil?)]
+       (filter not-nil? [nil :wheat nil "wheat" nil 'wheat nil])))
 
   "Partial functions allow procrastination"
   (= 20 (let [multiply-by-5 (partial * 5)]
@@ -19,8 +19,8 @@
 
   "Don't forget: first things first"
   (= [:a :b :c :d]
-       (let [ab-adder (partial concat [:a :b])]
-         (ab-adder [:c :d])))
+     (let [ab-adder (partial concat [:a :b])]
+       (ab-adder [:c :d])))
 
   "Functions can join forces as one 'composed' function"
   (= 25 (let [inc-and-square (comp square inc)]
@@ -28,7 +28,7 @@
 
   "Have a go on a double dec-er"
   (= 8 (let [double-dec (comp dec dec)]
-          (double-dec 10)))
+         (double-dec 10)))
 
   "Be careful about the order in which you mix your functions"
   (= 99 (let [square-and-dec (comp dec square)]
